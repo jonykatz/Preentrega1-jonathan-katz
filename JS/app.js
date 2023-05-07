17225f18eb44bc4445cae714470341351bd8877b
jonykatz <51973330+jonykatz@users.noreply.github.com>
2023-05-07 16:53:26 -0300

    first commit

---
+++ JS/app.js
@@ -0,0 +1,46 @@
+//addTicket
+
+let ticket = 0;
+let selectedTicket = '';
+let price = 0;
+
+function addTicket (show){
+    ticket ++    
+
+    if (show === selectedTicket || selectedTicket === '' ) {
+
+    selectedTicket = show;
+
+    switch (selectedTicket) {
+        case 'show1':
+            price = ticket * 3000;
+            var btn = document.querySelector("#checkoutBtn");
+                btn.textContent = 'COMPRAR: ' + ticket + ' Total a pagar $' + price;
+            break;
+
+        case 'show2':
+            price = ticket * 2000;
+            var btn = document.querySelector("#checkoutBtn");
+                btn.textContent = 'COMPRAR: ' + ticket + ' Total a pagar $' + price;
+            break;
+
+        case 'show3':
+            price = ticket * 2500;
+            var btn = document.querySelector("#checkoutBtn");
+                btn.textContent = 'COMPRAR: ' + ticket + ' Total a pagar $' + price;
+            break;
+        case 'show3':
+            price = ticket * 3500;
+            var btn = document.querySelector("#checkoutBtn");
+                btn.textContent = 'COMPRAR: ' + ticket + ' Total a pagar $' + price;
+            break;
+
+        default:
+            break;
+    }
+   } else {
+    alert('Ya tenes seleccionado otro evento ' + selectedTicket)
+   }
+          
+}
+
